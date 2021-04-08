3882046d8e9f3f3ffc9f4fbe24ad9a5e42952c21
gyounghun6612 <dev.gyounghun6612@gmail.com>
2021-04-08 23:15:13 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="AIS_gui",
+    version="0.0.1",
+    description="",
+    url="https://github.com/gyounghun6612/pyqt5_utils.git",
+    author="Choi_keonghun & Jun_eins",
+    author_email="dev.gyounghun6612@gmail.com",
+    packages=["ais_gui"],
+    zip_safe=False,
+    install_requires=[
+        "ais_utils"
+    ]
+)
